3f40c887834f699e20e21d36718de91a565c2b05
sportapora <henrysalim22@gmail.com>
2024-05-14 17:56:12 +0700

    fix CORS policy issue

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const cors = require("cors");
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
